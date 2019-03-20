7038220910fbcf817fa64c1dd830f9c313cd5b7d
Pomme1350 <league@wts22.attlocal.net>
2019-03-20 16:56:08 -0700

    module up

--- src/doubles_and_booleans/SleepyHead.java
+++ src/doubles_and_booleans/SleepyHead.java
@@ -1,9 +1,6 @@
 package doubles_and_booleans;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 
-//    Level 0
-
-import javax.print.attribute.standard.JobKOctetsProcessed;
 import javax.swing.JOptionPane;
 
 public class SleepyHead {
@@ -18,15 +15,24 @@ public class SleepyHead {
 		 * Ask the user for these values using a confirm dialog like the one below
 		 * JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head",
 		 * JOptionPane.YES_NO_OPTION);
-		 */JOptionPane.showConfirmDialog(null, "Is it a weekday?","Sleepy Head", JOptionPane.YES_NO_OPTION);
-		 	
+		 */
+		int weekday = JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
+		if (weekday == 0) {
+			isWeekday = true;
+		} else if (weekday == 1) {
+			isVacation = true;
+		}
 		/*
 		 * Print “sleep in�? if it is a vacation or a weekend. If it’s a weekday, print
 		 * “get up lazybones!�? If it is a weekday, and we are on vacation, print “sleep
 		 * in�?.
 		 */
-		 if(){
-			 
-		 }
+		if (isWeekday = true) {
+			JOptionPane.showMessageDialog(null, "Get up lazybones!");
+		}
+		if (isVacation = true) {
+			JOptionPane.showMessageDialog(null, "sleep in        zzzzzzz");
+		}
+
 	}
 }
